f9e1e5191c7ea8748ca44693fafe63b8ebb347f0
lutherkhill <luther.k.hill@gmail.com>
2021-03-05 12:49:34 -0500

    started ad integration

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+import hashlib
 import datetime
 from .models import Greeting
 import mailchimp_marketing as MailchimpMarketing
@@ -23,13 +24,25 @@ def subscribe(request):
   # function take email adress and  submitts it to mailchimp
   if request.method == 'POST':
         email = request.POST['email_id']
+        SUBSCRIBER_HASH = hashlib.md5(email.encode('utf-8')).hexdigest()
         member_info = {
 
             "email_address": email,
             "status": "subscribed",
+            "merge_fields": 
+            {
+                "tags": "Prudence",
+            }
         }
         try:
             mailchimp.lists.add_list_member(list_id, member_info)
+            mailchimp.lists.update_list_member_tags(list_id, SUBSCRIBER_HASH, {
+                "tags": 
+                [{
+                "name": "Influencer", #replace with campaign id
+                "status": "active"
+                }]
+            })
         except ApiClientError as error:
             print("An exception occurred: {}".format(error.text))
         html = "<html><body>Thank you for your interest %s.</body></html>" %email

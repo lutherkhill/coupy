3338007c8357840a3f7ec48775d08dd5f2f1c2d7
lutherkhill <luther.k.hill@gmail.com>
2021-03-07 16:42:51 -0500

    added basic url routing and views

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -116,5 +116,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 django_heroku.settings(locals())
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,20 +1,17 @@
 from django.urls import path, include
 from django.contrib import admin
 import hello.views as hello_views
+import photoStorage.views as photoStorage_views
 admin.autodiscover()
 
 # To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
-
 urlpatterns = [
     path("", hello_views.index),
     path("admin/", admin.site.urls),
     path('subscribe/', hello_views.subscribe, name='subscribe'),
+    path('photo/<str:pk>/', photoStorage_views.viewPhoto, name='photo'),
+    path('add/', photoStorage_views.addPhoto, name='add'),
+    path('gallery/', photoStorage_views.gallery, name='gallery'),
     
 ]
 
